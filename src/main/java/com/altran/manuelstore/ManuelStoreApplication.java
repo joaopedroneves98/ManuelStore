5dfb9e71fc4aae0c87c09d33071f6ff6c157d7ec
VASCONCELOS NEVES João Pedro <jpvasconcelosneves@europe.corp.altran.com>
2019-10-02 14:16:22 +0100

    Initial Commit

---
+++ src/main/java/com/altran/manuelstore/ManuelStoreApplication.java
@@ -0,0 +1,13 @@
+package com.altran.manuelstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManuelStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManuelStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/altran/manuelstore/ManuelStoreApplicationTests.java
@@ -0,0 +1,16 @@
+package com.altran.manuelstore;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ManuelStoreApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
